2ff5232fe4faf1e57438c05ef156624837403623
Richie <richie@aiii.ai>
2023-10-06 17:06:10 +0800

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'scss';
+
+  @ViewChild('container') container: ElementRef;
+
+  ngOnInit() {
+    // const container = document.querySelector(".container");
+    // const btn = document.querySelector("button");
+    // const preferenceQuery = window.matchMedia("(prefers-color-scheme: dark)");
+
+    // const addDarkMode = () => {
+    //   container.classList.remove("light-mode");
+    //   container.classList.add("dark-mode");
+    // };
+
+    // const addLightMode = () => {
+    //   container.classList.remove("dark-mode");
+    //   container.classList.add("light-mode");
+    // };
+
+    // const toggleTheme = () =>
+    //   !container.classList.contains("dark-mode") ? addDarkMode() : addLightMode();
+
+    // const checkPreference = () =>
+    //   preferenceQuery.matches ? addDarkMode() : addLightMode();
+
+    // btn.addEventListener("click", toggleTheme);
+    // this.preferenceQuery.addEventListener("change", checkPreference);
+    // window.addEventListener("DOMContentLoaded", checkPreference);
+    // (() => checkPreference())();
+
+    setTimeout(() => this.addLightMode());
+  }
+
+  addDarkMode() {
+    this.container.nativeElement.classList.remove("light-mode");
+    this.container.nativeElement.classList.add("dark-mode");
+  }
+
+  addLightMode() {
+    this.container.nativeElement.classList.remove("dark-mode");
+    this.container.nativeElement.classList.add("light-mode");
+  }
+
+  toggleTheme() {
+    !this.container.nativeElement.classList.contains("dark-mode") ? this.addDarkMode() : this.addLightMode();
+  }
+
 }
